adeb6f6cfb9f7cffb1fd24bca891a22e3cd9c957
Ilya Gromov <ilyagromow1@yandex.ru>
2024-04-17 10:36:44 +0300

    new project

---
+++ app/app/app.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+
+int EnterNX(int num_a) {
+//разрабатывается Ивановым И. - ветка branch_fun_1 return 0;
+}
+int EnterCX(int num_a) {
+    //разрабатывается Ивановым И. - ветка branch_fun_1 return 0;
+}
+void Calcproizv(int num_a) {
+    //разрабатывается Петровым П. - ветка branch_fun_2
+}
+void Calcsumm(int num_a) {
+    //разрабатывается Васильевым В. - ветка branch fun_3
+}
+
+int main()
+{
+   
+}
+
